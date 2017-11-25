93315c6aad65e3c1f2b2262de09d64c11a8bb1d0
dai <chamestica@gmail.com>
2017-11-25 18:56:45 +0900

    Use the component "nuxt-link" instead of "router-link"

--- nuxt/components/the-header.vue
+++ nuxt/components/the-header.vue
@@ -2,9 +2,9 @@
   <header class="the-header">
     <nav>
       <ul class="nav-list">
-        <li><router-link to="/">Top</router-link></li>
-        <li><router-link to="/a">Page A</router-link></li>
-        <li><router-link to="/b">Page B</router-link></li>
+        <li><nuxt-link to="/">Top</nuxt-link></li>
+        <li><nuxt-link to="/a">Page A</nuxt-link></li>
+        <li><nuxt-link to="/b">Page B</nuxt-link></li>
       </ul>
     </nav>
   </header>
